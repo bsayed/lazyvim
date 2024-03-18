13005895f1d50cff71e1bcd5be6a147b40ce8f6a
Bassam Sayed <bassam.sayed@toolsforhumanity.com>
2024-03-18 08:05:23 -0400

    Initial commit.

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,10 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- Enable inlay hints
+if vim.lsp.inlay_hint then
+  vim.keymap.set("n", "<leader>ti", function()
+    vim.lsp.inlay_hint(0, true)
+  end, { desc = "Toggle inlay hints" })
+end
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      setup = {
+        clangd = function(_, opts)
+          opts.capabilities.offsetEncoding = { "utf-16" }
+        end,
+      },
+      -- lspconfig enable inlay hints by default
+      inlay_hints = { enabled = true },
+      servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              hint = {
+                enable = true,
+              },
+            },
+          },
+        },
+        gopls = {
+          settings = {
+            Go = {
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
