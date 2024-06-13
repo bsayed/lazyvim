b977977f2a1a99b5c9851bf202c365609a5edad0
Bassam Sayed <bassam.sayed@toolsforhumanity.com>
2024-06-13 11:34:46 -0400

    Disabled the navic plugin and updated to latest.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,11 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Enable inlay hints
-if vim.lsp.inlay_hint then
-  vim.keymap.set("n", "<leader>ti", function()
-    vim.lsp.inlay_hint(0, true)
-  end, { desc = "Toggle inlay hints" })
-end
-
 vim.keymap.set("n", "<leader>p", '"_dp', { desc = "Paste without copying" })
