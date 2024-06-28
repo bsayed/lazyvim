fc82695cbe84ffb9bc389ed4be5fe54bece45b15
Bassam Sayed <bassam.sayed@toolsforhumanity.com>
2024-06-27 22:42:18 -0400

    Latest config.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -52,17 +52,17 @@ return {
     "ribru17/bamboo.nvim",
     lazy = false,
     priority = 1000,
-    config = function()
-      require("bamboo").setup({
-        -- optional configuration here
-      })
-      require("bamboo").load()
-    end,
+    -- config = function()
+    --   require("bamboo").setup({
+    --     -- optional configuration here
+    --   })
+    --   require("bamboo").load()
+    -- end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "bamboo",
+      colorscheme = "catppuccin-mocha",
     },
   },
 }
